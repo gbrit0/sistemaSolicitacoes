e31fba0495eb298879f8d3b0c5d97ac160f3791c
Gabriel Brito Ribeiro <gbrito341@gmail.com>
2024-12-14 09:21:15 -0300

    Lidando com dados

--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
+# Lógica pro trás das rotas
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-   return render(request, 'home/index.html')
+   dados = {
+      1: {
+         "nome": "Nebulosa de Carina",
+         "legenda": "webtelescope.org / NASA / James Webb"
+      },
+      2: {
+         "nome": "Galáxia NGC 1079",
+         "legenda":"nasa.org / NASA / Hubble"
+      }
+   }
+   return render(request, 'home/index.html', {"cards": dados})
 
 def imagem(request):
    return render(request, 'home/imagem.html')
